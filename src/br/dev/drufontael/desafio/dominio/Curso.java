b62a28aeb5c82f75021fe8cf01bc135d4ef948dc
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2024-07-24 14:31:24 -0300

    implementado herança

--- src/br/dev/drufontael/desafio/dominio/Curso.java
+++ src/br/dev/drufontael/desafio/dominio/Curso.java
@@ -1,30 +1,29 @@
 package br.dev.drufontael.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo {
+
     private int cargaHoraria;
 
-    public String getTitulo() {
-        return this.titulo;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return this.descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
     public int getCargaHoraria() {
-        return this.cargaHoraria;
+        return cargaHoraria;
     }
+
     public void setCargaHoraria(int cargaHoraria) {
         this.cargaHoraria = cargaHoraria;
     }
 
+
+    @Override
     public String toString() {
-        return "Curso [cargaHoraria=" + cargaHoraria + ", descricao=" + descricao + ", titulo=" + titulo + "]";
+        return "Curso{" +
+                "cargaHoraria=" + cargaHoraria +
+                ", titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                '}';
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 }
--- src/br/dev/drufontael/desafio/dominio/Mentoria.java
+++ src/br/dev/drufontael/desafio/dominio/Mentoria.java
@@ -2,31 +2,28 @@ package br.dev.drufontael.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo {
     private LocalDate data;
 
-    public String getTitulo() {
-        return this.titulo;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return this.descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
     public LocalDate getData() {
-        return this.data;
+        return data;
     }
     public void setData(LocalDate data) {
         this.data = data;
     }
-
-    public String toString() {
-        return "Mentoria [data=" + data + ", descricao=" + descricao + ", titulo=" + titulo + "]";
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO+20;
     }
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "data=" + data +
+                ", titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                '}';
+    }
+
+
+
 }
