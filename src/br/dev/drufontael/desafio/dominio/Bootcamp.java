a2137f6456be92be25c6a8deea3e6215674147c3
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2024-07-24 15:03:40 -0300

    Dev e Bootcamp

---
+++ src/br/dev/drufontael/desafio/dominio/Bootcamp.java
@@ -0,0 +1,39 @@
+package br.dev.drufontael.desafio.dominio;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Bootcamp {
+
+    private String nome;
+    private String descricao;
+    private Set<Dev> devsInscritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public Set<Dev> getDevsInscritos() {
+        return devsInscritos;
+    }
+    public void setDevsInscritos(Set<Dev> devsInscritos) {
+        this.devsInscritos = devsInscritos;
+    }
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+}
--- src/br/dev/drufontael/desafio/dominio/Conteudo.java
+++ src/br/dev/drufontael/desafio/dominio/Conteudo.java
@@ -1,5 +1,7 @@
 package br.dev.drufontael.desafio.dominio;
 
+import java.util.Objects;
+
 public abstract class Conteudo {
     protected static final double XP_PADRAO = 10d;
     private String titulo;
@@ -18,6 +20,23 @@ public abstract class Conteudo {
         this.descricao = descricao;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Conteudo conteudo = (Conteudo) o;
+
+        if (!Objects.equals(titulo, conteudo.titulo)) return false;
+        return Objects.equals(descricao, conteudo.descricao);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = titulo != null ? titulo.hashCode() : 0;
+        result = 31 * result + (descricao != null ? descricao.hashCode() : 0);
+        return result;
+    }
 
     public abstract double calcularXp();
 }
